1a8e718ea48ef6f4e9f898d907a8372e50cfbe67
usajavi0 <tu-correo-real@dominio.com>
2025-06-18 21:58:02 +0200

    fix: Ajusta la ruta base a relativa para el despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web-correo/', // <-- ASEGÚRATE DE QUE ESTO COINCIDE CON EL NOMBRE DE TU REPO
+  base: './', // <-- La ruta relativa, la solución definitiva
   build: {
     outDir: 'docs'
   }
