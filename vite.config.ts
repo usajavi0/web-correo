a5744c9085792ebd9e9e7d4776d19aab270dff6d
usajavi0 <tu-correo-real@dominio.com>
2025-06-18 21:44:14 +0200

    fix: Corrige la ruta base para que coincida con el nombre del repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/correo-web/', // <-- ASEGÚRATE DE QUE ESTO COINCIDE CON EL NOMBRE DE TU REPO
+  base: '/web-correo/', // <-- ASEGÚRATE DE QUE ESTO COINCIDE CON EL NOMBRE DE TU REPO
   build: {
     outDir: 'docs'
   }
